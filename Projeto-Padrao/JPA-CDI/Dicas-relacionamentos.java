8d7875f9107f35808bbc084cef8e0b75b1ed8100
marcosPAJava <mar.per.andr@gmail.com>
2015-04-20 19:49:54 -0300

    Update Dicas-relacionamentos.java

--- Projeto-Padrao/JPA-CDI/Dicas-relacionamentos.java
+++ Projeto-Padrao/JPA-CDI/Dicas-relacionamentos.java
@@ -49,3 +49,73 @@ Todo o relacionamento necessita que umas das entidades seja a “dona desse rela
 é do ter a chave estrangeira na tabela do banco de dados. No exemplo acima é possível ver na classe Call a utilização da anotação
 @JoinColumn.  Essa anotação indica que a chave estrangeira ficará dentro da tabela Call. O atributo mappedBy deve apontar para o 
 nome do atributo e não para o nome da classe.
+
+
+
+
+ManyToMany (muitos para muitos) Unidirecional e Bidirecional
+
+Um exemplo para um relacionamento ManyToMany poderia que uma pessoa pode ter vários cachorros e um cachorro pode ter várias pessoas
+(imagine um cachorro que more numa casa com 15 pessoas).Para a abordagem ManyToMany é necessário utilizar uma tabela adicional para 
+realizar a junção desse relacionamento. Teríamos uma tabela person (pessoa), uma tabela dog (cachorro) e uma tabela de 
+relacionamento chamada person_dog. Tabela person_dog teria apenas o id da pessoa, e o id cachorro indicando qual pessoa tem qual
+cachorro.
+
+@Entity
+public class Person {
+ 
+    @Id
+    @GeneratedValue
+    private int id;
+ 
+    private String name;
+ 
+    @ManyToMany
+    @JoinTable(name = "person_dog", joinColumns = @JoinColumn(name = "person_id"), 
+    inverseJoinColumns = @JoinColumn(name = "dog_id"))
+    private List<Dog> dogs;
+ 
+    @OneToOne
+    @JoinColumn(name = "cellular_id")
+    private Cellular cellular;
+ 
+    // get and set
+}
+
+Sobre o código acima:
+
+    A anotação @ManyToMany é utilizada para esse tipo de relacionamento. A anotação @JoinTable foi utilizada para definir qual 
+    tabela seria utilizada para realizar armazenas as entidades relacionadas. “name” define o nome da tabela. “joinColumns” define
+    qual será o nome da coluna na tabela da entidade que é a dona do relacionamento. “inverseJoinColumns” define qual será o nome 
+    da coluna  na tabela da entidade não dona do relacionamento.
+    
+    import java.util.List;
+ 
+import javax.persistence.*;
+ 
+@Entity
+public class Dog {
+ 
+    @Id
+    @GeneratedValue
+    private int id;
+ 
+    private String name;
+ 
+    @ManyToMany(mappedBy="dogs")
+    private List<Person> persons;
+ 
+    // get and set
+}
+
+É possível encontrar a notação @ManyToMany utilizando a opção mappedBy para determinar que a dona do relacionamento é a entidade 
+Person.Todo o relacionamento necessita que umas das entidades seja a “dona desse relacionamento”. No caso do relacionamento 
+ManyToMany a classe dona do relacionamento é a classe que não estiver utilizando a opção mappedBy na anotação @ManyToMany. 
+Caso nenhuma classe esteja marcada com mappedBy o JPA irá tratar cada classe marcada com ManyToMany como dona do relacionamento.
+O mappedBy deve apontar para o nome do atributo e não para o nome da classe.
+
+
+
+
+
+
